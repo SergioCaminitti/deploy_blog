7acc78f25cf62572a5e920724aad5f002e995048
SergioCaminitti <sergiocaminitti@gmail.com>
2025-01-29 11:58:30 -0300

    Configurando e iniciando o novo blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   get "up" => "rails/health#show", as: :rails_health_check
   get "inicio", controller: "home", action: "index", as: "home"
   get "estudante", to: "estudante#index", as: "estudantes"
